14f4db87a7697356a98d965ff2c523d50d4f1e8b
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-02 18:02:02 -0500

    Create functionality for section 2

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const testButton = document.querySelector("#test-button");
 const section1 = document.querySelector("#section-1");
-const section2 = document.querySelector("#section-2");
-const textInput = document.querySelector("#text-input");
-const printButton = document.querySelector("#print-button");
+const testButton = document.querySelector("#test-button");
 
 testButton.addEventListener("click", () => {
     const newText = document.createElement("p");
@@ -10,9 +7,16 @@ testButton.addEventListener("click", () => {
     section1.insertBefore(newText, testButton);
 });
 
+const section2 = document.querySelector("#section-2");
+const textInput = document.querySelector("#text-input");
+const printButton = document.querySelector("#print-button");
+
 printButton.addEventListener("click", () => {
     const input = textInput.value;
     const printInput = document.createElement("p");
     printInput.textContent = `${input}`;
     section2.appendChild(printInput);
 });
+
+const section3 = document.querySelector("#section-3");
+
